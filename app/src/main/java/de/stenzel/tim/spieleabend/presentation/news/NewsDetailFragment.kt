b81326b6d36f1a5cd3561607f1969e1dd94cb2aa
tim <tim.stenzel@uni-ulm.de>
2021-07-27 14:57:58 +0200

    added: news detail view

---
+++ app/src/main/java/de/stenzel/tim/spieleabend/presentation/news/NewsDetailFragment.kt
@@ -0,0 +1,52 @@
+package de.stenzel.tim.spieleabend.presentation.news
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.navArgs
+import com.google.firebase.storage.FirebaseStorage
+import dagger.hilt.android.AndroidEntryPoint
+import de.stenzel.tim.spieleabend.R
+import de.stenzel.tim.spieleabend.databinding.NewsDetailFragmentBinding
+import de.stenzel.tim.spieleabend.glide.GlideApp
+import de.stenzel.tim.spieleabend.helpers.formatDate
+
+@AndroidEntryPoint
+class NewsDetailFragment : Fragment() {
+
+    private var _binding : NewsDetailFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    private val args: NewsDetailFragmentArgs by navArgs()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = NewsDetailFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (args.img.isEmpty()) {
+            GlideApp.with(requireContext()).load(R.drawable.news_default).into(binding.newsDetailIv)
+        } else {
+            val ref = FirebaseStorage.getInstance().getReferenceFromUrl(args.img)
+            GlideApp.with(requireContext()).load(ref).error(R.drawable.error_default).into(binding.newsDetailIv)
+        }
+        binding.newsDetailPublisherTv.text = args.publisher
+        binding.newsDetailDateTv.text = formatDate(args.publishDate)
+        binding.newsDetailTitleTv.text = args.title
+        binding.newsDetailContentTv.text = args.content
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
